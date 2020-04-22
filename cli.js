21c40aabba7749d4124974d6a7859ba6a7619c8d
Shane Fontaine <shane6fontaine@gmail.com>
2020-04-21 20:10:50 -0700

    Add full CLI functionality

--- cli.js
+++ cli.js
@@ -7,8 +7,10 @@ const [,, ...args] = process.argv
 async function run() {
   const txHash = args[0]
   const network = args[1] || 'mainnet'
+  const blockNumber = args[2] || undefined
+  const provider = args[3] || undefined
 
-  console.log(await getRevertReason(txHash, network))
+  console.log(await getRevertReason(txHash, network, blockNumber, provider))
 }
 
 run()
